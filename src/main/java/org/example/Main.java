ab7085c4c8683292e8bab564f9a19bec433be80c
Ilia_Korostov <ilia.korostov@gmail.com>
2023-12-13 12:02:24 +0100

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ import static java.lang.String.format;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+        int a =5;
         int count =0;
         while (true) {
-            Thread.sleep(1000);
+            Thread.sleep(1000 + a);
             System.out.println(format("App running forever... %d", count++));
         }
     }
