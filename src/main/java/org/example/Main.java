0c775187bccf535fb4c1b49a9e93bd97e0328ad8
Ilia_Korostov <ilia.korostov@gmail.com>
2023-12-13 09:46:05 +0100

    forever :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
         int count =0;
         while (true) {
             Thread.sleep(2*1000);
-            System.out.println("Jetzt gehts los");
-            System.out.println(format("App running ... %d", count++));
+            System.out.println("Jetzt gehts los :)");
+            System.out.println(format("App running forever... %d", count++));
         }
     }
 }
